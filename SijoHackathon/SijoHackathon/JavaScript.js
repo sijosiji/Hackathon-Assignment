0f6fa89d2aec1cea2d5d1f2ad2c65a16119ce267
unknown <sijosoljacob@gmail.com>
2020-03-31 07:41:38 +0530

    Hackathon

---
+++ SijoHackathon/SijoHackathon/JavaScript.js
@@ -0,0 +1,23 @@
+﻿<script>
+class Hack {
+	constructor(base, height) {
+		this.base = base;
+		this.height = height;
+	}
+	}
+myHack = new Hack(6, 4);
+document.getElementById("AreaTriangle").innerHTML = ((myHack.base*myHack.height)/2);
+
+	function arrayOfMultiples(num, length) {
+		multiplication = [];
+
+	for (i = 1; i <= length; i++) {
+		multiplication.push(num * i);
+	}
+
+	return multiplication;
+}
+	document.getElementById("Multiple").innerHTML = (arrayOfMultiples(3,10));
+
+</script>
+
